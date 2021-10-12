81b2414f6dba8f1028af58e66d86aded2cd5d3ed
ido sobol <74209065+IdoSobol821@users.noreply.github.com>
2021-10-12 18:36:04 +0300

    Added comments to main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
+//importing the libraries:
 #include <Arduino.h>
 #include <Joystick.h>
-  
+#include <ultrasonic.h>
+
+//creating all of the objects:
   Joystick joystick;
-  
+  UltraSonic ultrasonic;
+
+
+  //defining all of the pins:
   const int buttonPin = 0;
   const int yPin = 0;
   const int xPin = 0;
+  const int trigPin = 0;
+  const int echoPin = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   joystick.JoystickInit(xPin , yPin , buttonPin);
+  ultrasonic.UltraSonicInit(trigPin , echoPin);
 }
 
 void loop() {
