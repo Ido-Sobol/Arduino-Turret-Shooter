363bf0a98d8f666a4bbd9358dc3e95bfed485532
ido sobol <74209065+IdoSobol821@users.noreply.github.com>
2021-10-12 17:51:05 +0300

    started ultrasonic

---
+++ lib/UltraSonic/ultrasonic.h
@@ -0,0 +1,15 @@
+#ifndef UltraSonic_h
+#define UltraSonic_h
+
+#include <Arduino.h>
+
+class UltraSonic
+{
+  public:
+   void UltraSonicInit(int trigPin , int echoPin);
+   double getDistance();
+  private:
+   int _trigPin;
+   int _echoPin;
+};
+#endif
