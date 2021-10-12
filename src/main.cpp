c40d934a0050cc270d7504b5f0a6d30c7252c388
ido sobol <74209065+IdoSobol821@users.noreply.github.com>
2021-10-12 17:17:07 +0300

    organized files

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <Arduino.h>
+#include <Joystick.h>
+  
+  Joystick joystick;
+  
+  const int buttonPin = 0;
+  const int yPin = 0;
+  const int xPin = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  joystick.JoystickInit(xPin , yPin , buttonPin);
 }
 
 void loop() {
